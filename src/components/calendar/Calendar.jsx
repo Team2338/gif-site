da5d50e8c22f3c84de4b88ec3f88db857b4dbc88
greenden007 <17001055@students.sd308.org>
2022-07-27 11:43:57 -0500

    Change from Contact to Calendar - still needs refinement on style

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,13 +1,32 @@
 import './calendar.scss';
 import { useState } from 'react';
-import Calendar from 'react-calendar';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from 'moment';
+
 
 function MyCalendar() {
     const [value, onChange] = useState(new Date());
+    const localizer = momentLocalizer(moment);
+    const myEventsList = [
+        {
+
+        }
+    ]
 
     return (
-        <div className="Calendar" id="calendar">
-            <Calendar onChange={onChange} value={value} />
+        <div className="MyCalendar" id="calendar">
+            <div className="left">
+                <h1 className='text-center'>Upcoming<br />Event<br />Calendar</h1>
+            </div>
+            <div className="container">
+                <Calendar
+                    localizer={localizer}
+                    className="tCal"
+                    events={myEventsList}
+                    startAccessor="start"
+                    endAccessor="end"
+                />
+            </div>
         </div>
     );
 }
