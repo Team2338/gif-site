accf3ca0d12ce99a00b917c587970c0398b712f1
rohancherukuri <rcherukuri10@gmail.com>
2022-08-29 21:35:13 -0500

    Small ui changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -14,6 +14,7 @@ function Intro() {
                 <AutoplaySlider
                     className="awesomeSlider"
                     play={true}
+                    organicArrows={false}
                     cancelOnInteraction={false}
                     interval={3500}
                     infinite={true}
