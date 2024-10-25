39dd863650e31871224589d9d646c3b841933330
Ronan Ludvigsen <ronanludvigsen@gmail.com>
2024-10-24 19:20:59 -0500

    Commited Photos

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,17 @@
 import './contact.scss';
-import Intro from "./Details/Intro.jsx";
+import Intro from "./Intro.jsx";
 import ContactForm from "./Details/ContactForm.jsx";
 
 function Contact(props) {
     return (
-        <React.Fragment>
+        <>
         <div className="contact">
             <Intro/>
             <div className="content">
                 <ContactForm/>
             </div>
             </div>
-        </React.Fragment>
+        </>
     );
 }
 
--- src/components/Subteams/subteams/MechanicalTeam.jsx
+++ src/components/Subteams/subteams/MechanicalTeam.jsx
@@ -7,7 +7,7 @@ function MechanicalTeam() {
         <section className="section after-blue mechaicalCaptain-section">
             <div className="content">
                 <div className="right">
-                    <img src="imgs/Mechanical2024.jpg" alt="Captain Photo" className="image-circle" />
+                    <img src="public\imgs\SubTeams\Mecahnical Ico.png" alt="Captain Photo" className="image-circle" />
                 </div>
                 <div className="left gray-part">
                     <h1 className="captain-title">Mechanical</h1>
